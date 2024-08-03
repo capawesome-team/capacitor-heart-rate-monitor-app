c2f539962b8ffdf04cfde1f2f8d3b640e95d33b7
Robin Genz <mail@robingenz.dev>
2024-08-03 16:12:09 +0200

    Add the implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit, signal } from '@angular/core';
+import { BluetoothLowEnergy } from '@capawesome-team/capacitor-bluetooth-low-energy';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public heartRate = signal<undefined | number>(undefined);
+
   constructor() {}
+
+  public async ngOnInit() {
+    await BluetoothLowEnergy.removeAllListeners();
+    // 1. Initialize the Bluetooth Low Energy plugin
+    await BluetoothLowEnergy.initialize();
+    // 2. Add a listener for the `deviceScanned` event
+    await BluetoothLowEnergy.addListener('deviceScanned', async (event) => {
+      if (event.name?.startsWith('Polar H9')) {
+        // 4. Stop scanning for devices
+        void BluetoothLowEnergy.stopScan();
+        // 5. Connect to the device
+        await BluetoothLowEnergy.connect({
+          deviceId: event.id,
+        });
+        // 6. Discover services
+        await BluetoothLowEnergy.discoverServices({
+          deviceId: event.id,
+        });
+        // 7. Add a listener for the `characteristicChanged` event
+        await BluetoothLowEnergy.addListener('characteristicChanged', (event) => {
+          let byteArray = new Uint8Array(event.value);
+          let firstBitValue = byteArray[0] & 0x01;
+          if (firstBitValue === 0) {
+            this.heartRate.set(byteArray[1]);
+          } else {
+            this.heartRate.set((byteArray[1] << 8) | byteArray[2]);
+          }
+        });
+        // 8. Start notifications for the Heart Rate Measurement characteristic
+        await BluetoothLowEnergy.startCharacteristicNotifications({
+          deviceId: event.id,
+          serviceId: '0000180D-0000-1000-8000-00805F9B34FB',
+          characteristicId: '00002A37-0000-1000-8000-00805F9B34FB',
+        });
+      }
+    });
+    // 3. Start scanning for devices
+    await BluetoothLowEnergy.startScan();
+  }
 }
