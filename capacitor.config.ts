37612638e1b48f78bc0d6b5271362e7df29ca48b
Robin Genz <mail@robingenz.dev>
2024-08-03 15:25:12 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-heart-rate-monitor',
+  webDir: 'www'
+};
+
+export default config;
